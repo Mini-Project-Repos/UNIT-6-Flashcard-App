070e84131010b66d5b76d57eac52893a515c01f0
Jamie Gobeille <djent34@gmail.com>
2020-03-06 16:08:04 +0900

    added pug

--- flashcards/app.js
+++ flashcards/app.js
@@ -2,10 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-  res.send("I love Treehouse!");
+  res.render("index");
 });
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.get("/hello", (req, res) => {
+  res.send("<h1>Hello JS Developer!</h1>");
+});
+
+//   process.env.PORT, process.env.IP
+app.listen(3000, () => {
   console.log("The app has started!");
 });
